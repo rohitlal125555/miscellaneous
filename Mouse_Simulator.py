18da238a62ff079a362edb69ac19d2fc42bf17ac
Rohit Lal <rohitlal.125555@gmail.com>
2024-02-02 17:31:20 +0530

    Added key-based application switching to avoid issues due to change in screen resolutions

--- Mouse_Simulator.py
+++ Mouse_Simulator.py
@@ -1,18 +1,50 @@
-
 import pyautogui as pgui
-import time, random
+import time
+import random
 
-x_corr_st = 487
-y_corr = 1050
-width = 68
+# config 
+enable_audio = False
+sleep_time_range = [30, 60]     # sleep time between each simulation
+taskbar_apps_range = [4, 10]    # range of apps on task bar to switch between. For eg: 1 - means 1st icon on taskbar.
+num_mouse_movements = [1, 4]    # number of intra-simulation mouse movements
 
-master_list = [2, 3, 4]
+if enable_audio: 
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice").Speak
+
+# screen resolution
+sWidth, sHeight = pgui.size()
+
+# easeInQuad     # start slow, end fast
+# easeOutQuad    # start fast, end slow
+# easeInOutQuad  # start and end fast, slow in middle
+# easeInBounce   # bounce at the end
+# easeInElastic  # rubber band at the end
+tweening_modes = [pgui.easeInOutQuad, pgui.easeInBounce, pgui.easeInOutBounce,
+                  pgui.easeInOutElastic, pgui.easeInOutSine]
+
+master_list = list(range(*taskbar_apps_range))
 current_choice = random.choice(master_list)
 
 while True:
-	slave_list = master_list.copy()
-	slave_list.remove(current_choice)
-	current_choice = random.choice(slave_list)
-	pgui.moveTo(x_corr_st + width * current_choice, y_corr)
-	pgui.click()
-	time.sleep(random.randint(40, 120))
+    slave_list = master_list.copy()
+    slave_list.remove(current_choice)    
+    current_choice = random.choice(slave_list)
+    how_many_mouse_movements = random.randint(*num_mouse_movements)
+    
+    if enable_audio:
+        speak(f"Choosing application {current_choice} and mouse movements {how_many_mouse_movements}")
+    
+    with pgui.hold('win'):  # Press the Shift key down and hold it.
+        pgui.press(str(current_choice))
+    
+    for move in range(how_many_mouse_movements):
+        pgui.moveTo(
+            random.randint(0, sWidth), 
+            random.randint(0, sHeight), 
+            random.randint(1, 3), 
+            random.choice(tweening_modes)
+        )
+        # pgui.click()
+    
+    time.sleep(random.randint(*sleep_time_range))
